777f4162ae86da1e2b1e01f9950de3fcdfeace34
Snipher Marube <sniphermarube@gmail.com>
2023-10-27 16:48:44 +0300

    serving static and media files

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from hello_world.core import views as core_views
 
@@ -23,3 +25,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

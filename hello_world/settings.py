d351270aa2111efe39b72e9d31c74a56cd9a8f91
Dishan Sachin <134765302+dishansa@users.noreply.github.com>
2023-11-14 07:15:35 +0530

    use python-dotenv to access environment variables

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

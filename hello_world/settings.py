de14e9499700d13ab270a88f8a4619d7bda915b5
martcpp <mordecaiemmanueletukudo@gmail.com>
2024-03-12 12:54:59 +0100

    added a script tto generate serect key

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('my_secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")

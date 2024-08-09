c300c194092c3d0c0c045e376f23ef73325c9773
CrabCatcher0 <rautprashant103@gmail.com>
2024-08-10 00:18:09 +0545

    SECRETE_KEY automatically generated.

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", default='')
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True)
